6e85c9aa82cc822eb699e91be5ee64305c8ec1cd
Kevin Petschulat <kevin.petschulat@forsprung.com>
2018-02-01 15:05:57 +0100

    added simple integration test for history endpoint

--- NetflixRouletteBackend/src/test/java/at/fhv/netflix/endpoints/HistoryEndpointIT.java
+++ NetflixRouletteBackend/src/test/java/at/fhv/netflix/endpoints/HistoryEndpointIT.java
@@ -1,9 +1,19 @@
 package at.fhv.netflix.endpoints;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 //integration test for history endpoint
 public class HistoryEndpointIT {
 
+	@Test(expected = Exception.class)
+	public void testGetHistoryNullToken() throws Exception {
+		HistoryEndpoint endpoint = new HistoryEndpoint();
+		endpoint.getHistory(null);
+	}
+	
+	@Test(expected = Exception.class)
+	public void testGetHistoryEmptyToken() throws Exception {
+		HistoryEndpoint endpoint = new HistoryEndpoint();
+		endpoint.getHistory("");
+	}
 }
