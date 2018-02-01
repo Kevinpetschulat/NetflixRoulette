77628a639c83403776ba2e9ca93bb96fd60ce4d6
Stefan T <stefan.traut@students.fhv.at>
2018-02-01 10:37:29 +0100

    update styles

--- NetflixRouletteFrontend/assets/js/app.js
+++ NetflixRouletteFrontend/assets/js/app.js
@@ -37,7 +37,7 @@ $(function() {
                       for (var i = 0; i < response.items.length; i++) {
                         var movie = response.items[i].result;
                         $("#history").append('<article class="6u 12u$(xsmall) work-item">' +
-                                '<a href="images/fulls/01.jpg" class="image fit thumb"><img src="' + movie.imageUrl + '" title="' + movie.movieName + '" /></a>' +
+                                '<img src="' + movie.imageUrl + '" title="' + movie.movieName + '"  class="image fit thumb" />' +
                                 '<h3>' + movie.movieName + '</h3>' +
                                 '<p>' + movie.description + '</p>' +
                             '</article>');
